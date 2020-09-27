40c1102ffecfe37ca5a8803fd79463f567b324f5
Huy Nguyen Gia <giahuy01234@gmail.com>
2020-09-27 23:56:14 +0700

    update footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,20 +1,83 @@
 import React from 'react'
+import Logo from './Logo'
 import './Footer.css'
 
-export default () => (
-  <div>
-    <h2 className="taCenter">
-      Follow us{' '}
-      <a href="https://instagram.com/thrivegoldcoast/">@thrivegoldcoast</a>
-    </h2>
-    <br />
+export default () => {
+  const copyright = ' ALL RIGHT RESERVED. MADE BY TRÒN HOUSE 2019',
+        address = [
+          {
+            iconName: 'map-marker',
+            label: 'THÔNG TIN LIÊN HỆ',
+            info: '292 NGUYỄN VĂN LINH, PHƯỜNG BÌNH THUẬN, QUẬN 7, HCM'
+          }, {
+            iconName: 'phone',
+            label: 'HOTLINE',
+            info: '+84 961 934 065'
+          }, {
+            iconName: 'envelope',
+            label: 'HELLO@TRONHOUSE.COM',
+            socials: [
+              {
+                url: '#',
+                iconName: 'facebook'
+              }, {
+                url: '#',
+                iconName: 'instagram'
+              }, {
+                url: '#',
+                iconName: 'youtube'
+              }, {
+                url: '#',
+                iconName: 'pinterest'
+              }, {
+                url: '#',
+                iconName: 'vimeo'
+              }, {
+                url: '#',
+                iconName: 'behance'
+              }
+            ]
+          }
+        ]
+
+  return (
     <footer className="footer">
-      <div className="container taCenter">
-        <span>
-          © Copyright {new Date().getFullYear()} All rights reserved. Crafted by{' '}
-          <a href="https://thriveweb.com.au/">Thrive</a>.
-        </span>
+      <div className="footer-top">
+        <div className="container">
+          <div className="inner">
+            <Logo isFooter={true} />
+            <div className="address">
+              {!!address && address.map((item, index) => (
+                <div className="item" key={`address-${index}`}>
+                  <div className="label">
+                    <span className={`fa fa-${item.iconName}`}></span>
+                    <span className="text">{item.label}</span>
+                  </div>
+                  {item.info && (
+                    <p className="info">{item.info}</p>
+                  )}
+                  {!!item.socials && (
+                    <div className="list-social">
+                      {item.socials.map((socialItem, index) => (
+                        <a href={socialItem.url} className="link-social">
+                          <span className={`fa fa-${socialItem.iconName}`}></span>
+                        </a>
+                      ))}
+                    </div>
+                  )}
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="footer-bottom">
+        <div class="container">
+          <div class="inner">
+            <p class="copyright">&copy;{copyright}</p>
+          </div>
+        </div>
       </div>
     </footer>
-  </div>
-)
+  )
+}
--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import './Logo.css'
 
-export default () => (
-  <div className="Logo">
-    <img src="images/logo.gif" alt="" />
-  </div>
-)
+export default ({ isFooter }) => {
+  const footerClass = isFooter ? 'logo-footer' : ''
+
+  return (
+    <div className={`Logo ${footerClass}`}>
+      <img src="images/logo.gif" alt="" />
+    </div>
+  )
+}
